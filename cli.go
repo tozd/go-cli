68ce8202afe4bece50671c2851e045a403819072
Mitar <mitar.git@tnode.com>
2023-10-17 23:38:24 +0200

    Initial version.

---
+++ cli.go
@@ -0,0 +1,137 @@
+// Package cli provides boilerplate combining [Kong] CLI argument parsing with
+// [zerolog] logging.
+//
+// [Kong]: https://github.com/alecthomas/kong
+// [zerolog]: https://gitlab.com/tozd/go/zerolog
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/rs/zerolog/log"
+	"gitlab.com/tozd/go/errors"
+	"gitlab.com/tozd/go/zerolog"
+)
+
+// These variables should be set during build time using "-X" ldflags.
+// They are then combined into a version string and provided as [Kong] variable
+// with name "version". The variable is then used by kong.VersionFlag
+// to show program's version. For example, to have "-v" CLI flag show program's
+// version, your Kong config struct could be:
+//
+//	type Config struct {
+//		Version kong.VersionFlag `short:"V" help:"Show program's version and exit." json:"-" yaml:"-"`
+//	}
+//
+// [Kong]: https://github.com/alecthomas/kong
+var (
+	Version        = ""
+	BuildTimestamp = ""
+	Revision       = ""
+)
+
+const (
+	// Exit code 1 is used by Kong, 2 when program panics, 3 when program returns an error.
+	errorExitCode = 3
+)
+
+type fmtError struct {
+	Err error
+}
+
+func (e *fmtError) Error() string {
+	return fmt.Sprintf("% -+#.1v", errors.Formatter{Error: e.Err}) //nolint:exhaustruct
+}
+
+func (e *fmtError) Unwrap() error {
+	return e.Err
+}
+
+// Run runs the "run" function after [Kong] parses CLI arguments into "config" struct
+// and [zerolog] logging is configured and Logger and Logger WithContext fields are set
+// in "config" struct.
+//
+// Kong vars can override zerolog defaults and add additional variables which can then
+// be interpolated in Kong struct tags in config struct.
+//
+// Run function should always return and never call os.Exit. If it does not return
+// an error, the program exits with code 0. If it returns an error, the program exits
+// with code 3. The program exits with code 1 when CLI argument parsing or zerolog
+// configuration fails. The program exits with code 2 on panic.
+//
+// Run function should not do any output to stdout by itself, but should exclusively
+// use the logger. Logger then uses stdout for pretty-printed or JSON logging
+// (as configured).
+// Any unexpected errors go to stderr and are not in any particular format
+// nor JSON (e.g., stack traces on panic).
+// This combines well with [dinit].
+//
+// [Kong]: https://github.com/alecthomas/kong
+// [zerolog]: https://gitlab.com/tozd/go/zerolog
+// [dinit]: https://gitlab.com/tozd/dinit
+func Run(config interface{}, description string, vars kong.Vars, run func(*kong.Context) errors.E) {
+	// Inside this function, panicking should be set to false before all regular returns from it.
+	panicking := true
+
+	parser, err := kong.New(config,
+		kong.Description(description),
+		kong.UsageOnError(),
+		kong.Writers(
+			os.Stderr,
+			os.Stderr,
+		),
+		kong.Vars{
+			"version":                               fmt.Sprintf("version %s (build on %s, git revision %s)", Version, BuildTimestamp, Revision),
+			"defaultLoggingConsoleType":             zerolog.DefaultConsoleType,
+			"defaultLoggingConsoleLevel":            zerolog.DefaultConsoleLevel,
+			"defaultLoggingFileLevel":               zerolog.DefaultFileLevel,
+			"defaultLoggingMainLevel":               zerolog.DefaultMainLevel,
+			"defaultLoggingContextLevel":            zerolog.DefaultContextLevel,
+			"defaultLoggingContextConditionalLevel": zerolog.DefaultContextConditionalLevel,
+			"defaultLoggingContextTriggerLevel":     zerolog.DefaultContextTriggerLevel,
+		}.CloneWith(vars),
+		zerolog.KongLevelTypeMapper,
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: % -+#.1v", err)
+		os.Exit(1)
+	}
+
+	ctx, err := parser.Parse(os.Args[1:])
+	if err != nil {
+		// We use FatalIfErrorf here because it displays usage information. But we use
+		// fmtError instead of err so that we format the error and add more details
+		// through its Error method, which is called inside FatalIfErrorf.
+		parser.FatalIfErrorf(&fmtError{err})
+	}
+
+	// Default exist code.
+	exitCode := 0
+	defer func() {
+		if !panicking {
+			os.Exit(exitCode)
+		}
+	}()
+
+	logFile, errE := zerolog.New(&config)
+	if logFile != nil {
+		defer logFile.Close()
+	}
+	if errE != nil {
+		parser.Fatalf("% -+#.1v", errE)
+	}
+
+	// We access main logger through global zerolog logger here, which was set in New.
+	// This way we do not have to know anything about the config structure.
+	logger := log.Logger
+
+	errE = run(ctx)
+	if errE != nil {
+		logger.Error().Err(errE).Send()
+		exitCode = errorExitCode
+	}
+
+	panicking = false
+}
---
+++ config.go
@@ -0,0 +1,59 @@
+package cli
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/alecthomas/kong"
+	"gitlab.com/tozd/go/errors"
+	"gopkg.in/yaml.v3"
+)
+
+// ConfigFlag allows you to define a config struct passed to [Kong]
+// to parse a CLI argument with the path to the config file to populate
+// the config struct with its contents.
+//
+// CLI arguments and environment variables can then override values
+// populated by the config file.
+//
+// Config file is parsed with a YAML parser so it should be in YAML or JSON.
+// Make sure the config struct supports YAML parser to populate it
+// (use  "yaml" struct tags, implement [UnmarshalYAML]
+// if custom parsing is needed, etc.).
+//
+// Example:
+//
+//	type Config struct {
+//		Config cli.ConfigFlag `short:"c" name:"config" placeholder:"PATH" help:"Load configuration from a JSON or YAML file." json:"-" yaml:"-"`
+//	}
+//
+// [Kong]: https://github.com/alecthomas/kong
+// [UnmarshalYAML]: https://pkg.go.dev/gopkg.in/yaml.v3#Unmarshaler
+type ConfigFlag string
+
+func (c ConfigFlag) BeforeResolve(app *kong.Kong, ctx *kong.Context, trace *kong.Path) error {
+	path := string(ctx.FlagValue(trace.Flag).(ConfigFlag))
+	file, err := os.Open(kong.ExpandPath(path))
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer file.Close()
+	decoder := yaml.NewDecoder(file)
+	decoder.KnownFields(true)
+	err = decoder.Decode(app.Model.Target.Addr().Interface())
+	if err != nil {
+		var yamlErr *yaml.TypeError
+		if errors.As(err, &yamlErr) {
+			e := "error"
+			if len(yamlErr.Errors) > 1 {
+				e = "errors"
+			}
+			return errors.Errorf("yaml: unmarshal %s: %s", e, strings.Join(yamlErr.Errors, "; "))
+		} else if errors.Is(err, io.EOF) {
+			return nil
+		}
+		return errors.WithStack(err)
+	}
+	return nil
+}

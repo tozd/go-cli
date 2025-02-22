7da71da69d51ff578d9bd6fe3430983ab947e610
Mitar <mitar.git@tnode.com>
2025-02-23 00:40:20 +0100

    Kong now works with variables in default.

--- _examples/single.go
+++ _examples/single.go
@@ -14,9 +14,9 @@ const DefaultMessage = "Hello world!"
 type App struct {
 	zerolog.LoggingConfig `yaml:",inline"`
 
-	Version kong.VersionFlag `                                   help:"Show program's version and exit."                                                              short:"V" yaml:"-"`
-	Config  cli.ConfigFlag   `                                   help:"Load configuration from a JSON or YAML file."   name:"config"             placeholder:"PATH"   short:"c" yaml:"-"`
-	Message string           `arg:"" default:"${defaultMessage}" help:"Message to output. Default: ${defaultMessage}."               optional:"" placeholder:"STRING"           yaml:"message"`
+	Version kong.VersionFlag `                                   help:"Show program's version and exit."                                                            short:"V" yaml:"-"`
+	Config  cli.ConfigFlag   `                                   help:"Load configuration from a JSON or YAML file." name:"config"             placeholder:"PATH"   short:"c" yaml:"-"`
+	Message string           `arg:"" default:"${defaultMessage}" help:"Message to output. Default: ${default}."                    optional:"" placeholder:"STRING"           yaml:"message"`
 }
 
 func main() {
